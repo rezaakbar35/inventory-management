0a2a4a418ffd3f1b3f6fd2cfd90bcc2ceb038a3b
Bachry28 <muhbahryalhadi@gmail.com>
2023-11-23 00:53:26 +0700

    add modify use controller and routes User Login and Register

--- controller/userRegisterController.js
+++ controller/userRegisterController.js
@@ -1,7 +1,7 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+
 
 async function createUserRegister(req, res) {
     
