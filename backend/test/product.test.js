9de8f10d029c03cc7312421fc500ba1e455851dd
riristreamlit <rezaakbarattariq@gmail.com>
2023-11-25 01:33:27 +0700

    update automated API test feature

--- backend/test/product.test.js
+++ backend/test/product.test.js
@@ -1,37 +1,44 @@
-const app = require('../app')
-const request = require('supertest')
+const app = require('../app');
+const request = require('supertest');
 const path = require('path');
 
 describe('Product Test', () => {
     
-    //test post method
-    // test('product create test', (done) => {
-    //     const newProduct = {
-    //         product_code: 2001,
-    //         product_name: "Baby Doll",
-    //         category_id: 1,
-    //         product_stock: 1,
-    //         warehouse_id: 1,
-    //         product_image: "/uploads/img.jpg",
-    //         product_status: "di gudang",
-    //         arrival_at: new Date(),
-    //     }
+    // Test the combined product creation and image upload
+    test('product create and image upload test', (done) => {
+        const imagePath = path.resolve(__dirname, 'C:/Users/LENOVO/Downloads/aaaa.jpg');
 
-    //     request(app)
-    //         .post('/product/create')
-    //         .field('complex_object', '{product_code: 2001, product_name: "Baby Doll", category_id: 1, product_stock: 1, warehouse_id: 1, product_status: "di gudang",}', {contentType: 'application/json'})
-    //         .attach('product_image', '"C:\Users\LENOVO\Downloads\aaaa.jpg"')
-    //         .expect('Content-Type', /json/)
-    //         .then(response => {
-    //             expect(response.body.message).toBe('Succesfully Create New Product!')
-    //             expect(response.status).toBe(201)
-    //             done()
-    //         })
-    //         .catch(done)
-    // })
+        const newProduct = {
+            product_code: 2001,
+            product_name: 'Baby Doll',
+            category_id: 1,
+            product_stock: 1,
+            warehouse_id: 1,
+            product_status: 'di gudang',
+        };
 
-    //tambahkan test baru dibawah ini
+        request(app)
+            .post('/product/create')
+            .field('product_code', String(newProduct.product_code))
+            .field('product_name', newProduct.product_name)
+            .field('category_id', String(newProduct.category_id))
+            .field('product_stock', String(newProduct.product_stock))
+            .field('warehouse_id', String(newProduct.warehouse_id))
+            .field('product_status', newProduct.product_status)
+            .attach('product_image', imagePath)
+            .expect('Content-Type', /json/)
+            .expect(201)
+            .timeout(60000)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                expect(res.body.message).toBe('Succesfully Create New Product!');
+                done();
+            });
+    });
 
-    //diatas ini
+    // Add new tests below this comment
 
-})
+    // Above this comment
+});
