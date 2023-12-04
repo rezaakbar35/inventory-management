74b4890718dc671bed6f930892ae49f54830515d
Bachry28 <muhbahryalhadi@gmail.com>
2023-12-04 23:39:52 +0700

    Modify Table Product

--- frontend/src/components/TableProduct.jsx
+++ frontend/src/components/TableProduct.jsx
@@ -1,90 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BsFillTrashFill, BsFillPencilFill } from "react-icons/bs";
 import './TableProduct.css'
+import { deleteProduct, getAllProduct } from "../modules/fetch/product";
 
 export const TableProduct = ({}) => {
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = () => {
+    setLoading(true);
+    getAllProduct().then((result) => {
+      setData([...result.product]);
+      setLoading(false)
+    })
+  }
+
+  const editRow = (id) => {
+    console.log(id)
+  }
+
+  const deleteRow =  (id) => {
+     deleteProduct(id).then((response) => {
+      console.log(response)
+      fetchData();
+    })
+  }
+
   return (
-    <div className="table-wrapper overflow-x-auto">
-      <table className="min-w-full divide-y divide-gray-200">
-        <thead className="bg-gray-100">
-          <tr>
-            <th className="px-6 py-3 text-centre text-xs font-Large text-gray-500 uppercase tracking-wider">ID</th>
-            <th className="px-6 py-3 text-centre text-xs font-Large text-gray-500 uppercase tracking-wider">Product Name</th>
-            <th className="px-6 py-3 text-centre text-xs font-Large text-gray-500 uppercase tracking-wider">Product Code</th>
-            <th className="px-6 py-3 text-centre text-xs font-Large text-gray-500 uppercase tracking-wider">Warehouse</th>
-            <th className="px-6 py-3 text-centre text-xs font-Large text-gray-500 uppercase tracking-wider">Arrrival Date</th>
-            <th className="px-6 py-3 text-centre text-xs font-Large text-gray-500 uppercase tracking-wider">Action</th>
-          </tr>
-        </thead>
-        <tbody className="bg-white divide-y divide-gray-200 text-black">
-              <tr  className="hover:bg-gray-50">
-                <td className="px-6 py-2 whitespace-nowrap">1</td>
-                <td className="px-6 py-2 whitespace-nowrap">Baby Gum</td>
-                <td className="px-6 py-2 whitespace-nowrap">11210</td>
-                <td className="px-6 py-2 whitespace-nowrap">Toys Warehouse</td>
-                <td className="px-6 py-2 whitespace-nowrap">02.00 PM 12/12/23</td>
-                <td className="px-6 py-2 whitespace-nowrap">
-               <div className="flex items-center justify-center space-x-7">
-              <BsFillTrashFill className="text-red-500 cursor-pointer" onClick={() => deleteRow()} />
-              <BsFillPencilFill className="text-blue-500 cursor-pointer" onClick={() => editRow()} />
-              </div>
-              </td>
+    (
+      loading ?
+      (
+        <div>Loading ..</div>
+      ) :
+      (
+        <div className="table-wrapper overflow-x-auto">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-100">
+            <tr>
+              <th className="px-6 py-3 text-centre text-xs font-Large text-gray-500 uppercase tracking-wider">ID</th>
+              <th className="px-6 py-3 text-centre text-xs font-Large text-gray-500 uppercase tracking-wider">Product Name</th>
+              <th className="px-6 py-3 text-centre text-xs font-Large text-gray-500 uppercase tracking-wider">Product Code</th>
+              <th className="px-6 py-3 text-centre text-xs font-Large text-gray-500 uppercase tracking-wider">Warehouse</th>
+              <th className="px-6 py-3 text-centre text-xs font-Large text-gray-500 uppercase tracking-wider">Arrrival Date</th>
+              <th className="px-6 py-3 text-centre text-xs font-Large text-gray-500 uppercase tracking-wider">Action</th>
             </tr>
-              <tr  className="hover:bg-gray-50">
-                <td className="px-6 py-2 whitespace-nowrap">2</td>
-                <td className="px-6 py-2 whitespace-nowrap">Baby Gum</td>
-                <td className="px-6 py-2 whitespace-nowrap">11210</td>
-                <td className="px-6 py-2 whitespace-nowrap">Toys Warehouse</td>
-                <td className="px-6 py-2 whitespace-nowrap">02.00 PM 12/12/23</td>
-                <td className="px-6 py-2 whitespace-nowrap">
-               <div className="flex items-center justify-center space-x-7">
-              <BsFillTrashFill className="text-red-500 cursor-pointer" onClick={() => deleteRow()} />
-              <BsFillPencilFill className="text-blue-500 cursor-pointer" onClick={() => editRow()} />
-              </div>
-              </td>
-            </tr>
-              <tr  className="hover:bg-gray-50">
-                <td className="px-6 py-2 whitespace-nowrap">3</td>
-                <td className="px-6 py-2 whitespace-nowrap">Baby Gum</td>
-                <td className="px-6 py-2 whitespace-nowrap">11210</td>
-                <td className="px-6 py-2 whitespace-nowrap">Toys Warehouse</td>
-                <td className="px-6 py-2 whitespace-nowrap">02.00 PM 12/12/23</td>
-                <td className="px-6 py-2 whitespace-nowrap">
-               <div className="flex items-center justify-center space-x-7">
-              <BsFillTrashFill className="text-red-500 cursor-pointer" onClick={() => deleteRow()} />
-              <BsFillPencilFill className="text-blue-500 cursor-pointer" onClick={() => editRow()} />
-              </div>
-              </td>
-            </tr>
-              <tr  className="hover:bg-gray-50">
-                <td className="px-6 py-2 whitespace-nowrap">4</td>
-                <td className="px-6 py-2 whitespace-nowrap">Baby Gum</td>
-                <td className="px-6 py-2 whitespace-nowrap">11210</td>
-                <td className="px-6 py-2 whitespace-nowrap">Toys Warehouse</td>
-                <td className="px-6 py-2 whitespace-nowrap">02.00 PM 12/12/23</td>
-                <td className="px-6 py-2 whitespace-nowrap">
-               <div className="flex items-center justify-center space-x-7">
-              <BsFillTrashFill className="text-red-500 cursor-pointer" onClick={() => deleteRow()} />
-              <BsFillPencilFill className="text-blue-500 cursor-pointer" onClick={() => editRow()} />
-              </div>
-              </td>
-            </tr>
-              <tr  className="hover:bg-gray-50">
-                <td className="px-6 py-2 whitespace-nowrap">5</td>
-                <td className="px-6 py-2 whitespace-nowrap">Baby Gum</td>
-                <td className="px-6 py-2 whitespace-nowrap">11210</td>
-                <td className="px-6 py-2 whitespace-nowrap">Toys Warehouse</td>
-                <td className="px-6 py-2 whitespace-nowrap">02.00 PM 12/12/23</td>
-                <td className="px-6 py-2 whitespace-nowrap">
-               <div className="flex items-center justify-center space-x-7">
-              <BsFillTrashFill className="text-red-500 cursor-pointer" onClick={() => deleteRow()} />
-              <BsFillPencilFill className="text-blue-500 cursor-pointer" onClick={() => editRow()} />
-              </div>
-              </td>
-            </tr>
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200 text-black">
+              {
+                data.map((item) => (
+                  <tr  className="hover:bg-gray-50">
+                    <td className="px-6 py-2 whitespace-nowrap">{ item.product_id}</td>
+                    <td className="px-6 py-2 whitespace-nowrap">{ item.product_name}</td>
+                    <td className="px-6 py-2 whitespace-nowrap">{ item.product_code }</td>
+                    <td className="px-6 py-2 whitespace-nowrap">{ item.warehouse_id }</td>
+                    <td className="px-6 py-2 whitespace-nowrap">{ item.arrival_at } </td>
+                    <td className="px-6 py-2 whitespace-nowrap">
+                  <div className="flex items-center justify-center space-x-7">
+                  <BsFillTrashFill className="text-red-500 cursor-pointer" onClick={() => deleteRow(item.product_id)} />
+                  <BsFillPencilFill className="text-blue-500 cursor-pointer" onClick={() => editRow(item.product_id)} />
+                  </div>
+                  </td>
+                </tr>
+                ))
+              }
+          </tbody>
+        </table>
+      </div>
+      )
+    )
+
   );
 };
 
